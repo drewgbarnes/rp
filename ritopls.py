ace7592093a50f069424f4e9e150464e6470f8b3
Drew Barnes <drew.g.barnes@gmail.com>
2016-06-06 16:20:00 -0700

    Update ritopls.py

--- ritopls.py
+++ ritopls.py
@@ -16,7 +16,7 @@
 #  play better with specific champions
 
 
-import requests, json, time, itertools, collections, ast
+import requests, json, time, itertools, collections, ast, datetime
 
 
 def convert(data):
@@ -78,37 +78,51 @@ def get_match(match_id):
 	match = rito_pls('v2.2/match/', match_id)
 	return match
 
-def get_one_matchlist(summs):
+def get_one_matchlist(summs, name):
 	for summ in summs:
-		matchlist = get_matchlist(summs[summ]['id'])
-		if matchlist:
-			return matchlist
+		if summ == name:
+			matchlist = get_matchlist(summs[summ]['id'])
+			if matchlist:
+				return matchlist
 
 def run():
+	# do not do cache false and all true
 	CACHE = True
+	ALL = True
+	primary_summ = 'slawcat'
 	summs_to_compare = sorted(['mehabahaha','rockologist','slawcat'])
 	desired_stats = ['killingSprees','neutralMinionsKilled','largestMultiKill','towerKills','kills','totalDamageDealtToChampions','wardsPlaced','wardsKilled','deaths','assists','goldEarned','totalDamageTaken','minionsKilled']
 	
 	summs_combos = list(itertools.combinations(summs_to_compare, r=2))
 	summs_combos.append(tuple(summs_to_compare))
-	summs_combos.append((summs_to_compare[0],))
+	if ALL:
+		for summ in summs_to_compare:
+			summs_combos.append((summ,))
+	else:
+		summs_combos.append((primary_summ,))
 
 	summs={u'mehabahaha': {u'profileIconId': 688, u'summonerLevel': 30, u'revisionDate': 1464378381000, u'id': 41938767, u'name': u'mehabahaha'}, u'slawcat': {u'profileIconId': 682, u'summonerLevel': 30, u'revisionDate': 1464704976000, u'id': 41261356, u'name': u'slawcat'}, u'rockologist': {u'profileIconId': 786, u'summonerLevel': 30, u'revisionDate': 1464653929000, u'id': 44154717, u'name': u'rockologist'}}
-	# summs = get_summoners(summs_to_compare)
-
-	matchlist = get_one_matchlist(summs)
+	# summs = get_summoners(summs_to_compare)		
 
 	matches = {}
 	if CACHE:
-		with open('matches.txt', 'r') as destination:
-			matches = ast.literal_eval(destination.read())
+		if ALL:
+			for summ in summs_to_compare:
+				with open(summ + '_matches.txt', 'r') as destination:
+					matches.update(ast.literal_eval(destination.read()))
+		else:
+			with open(primary_summ + '_matches.txt', 'r') as destination:
+				matches = ast.literal_eval(destination.read())
 	else:
+		matchlist = get_one_matchlist(summs, primary_summ)
 		for m in matchlist['matches']:
 			match = get_match(m['matchId'])
 			if 'participantIdentities' in match:
 				matches[match['matchId']] = match
 
-	with open('matches.txt', 'w') as destination:
+	if ALL:
+		primary_summ = 'ALL'
+	with open(primary_summ + '_matches.txt', 'w') as destination:
 		destination.write(str(matches))
 
 	matches_to_players = {}
@@ -126,7 +140,10 @@ def run():
 	for pair in summs_combos:
 		averages[pair] = {}
 		champs_played[pair] = {}
-		total_games[pair] = 0.0
+		total_games[pair] = {}
+		total_games[pair]['total'] = 0.0
+		total_games[pair]['durations'] = 0.0
+		total_games[pair]['wins'] = 0.0
 		for summ in summs:
 			if summ in pair:
 				champs_played[pair][summ] = {}
@@ -136,6 +153,7 @@ def run():
 
 	known_champs = {}
 	nprocessed = 0
+	oldest_game_id = matches_to_players.keys()[0]
 	for mid in matches:
 		print('processing match %d' % nprocessed)
 		nprocessed += 1
@@ -143,7 +161,14 @@ def run():
 		for player in matches_to_players[mid]:
 			names.append(player[0])
 		pair = tuple(sorted(names))
-		total_games[pair] += 1
+		total_games[pair]['total'] += 1
+		total_games[pair]['durations'] += matches[mid]['matchDuration']
+		date = datetime.datetime.fromtimestamp(matches[mid]['matchCreation']/1000.0)
+		
+		if date < datetime.datetime.fromtimestamp(matches[oldest_game_id]['matchCreation']/1000.0):
+			oldest_game_id = mid
+
+		checkwin = False
 		for player in matches[mid]['participants']:
 			pid = player['participantId']
 			cid = player['championId']
@@ -160,14 +185,28 @@ def run():
 
 					for stat in desired_stats:
 						averages[pair][playa[0]][stat] += player['stats'][stat]
+					if player['stats']['winner'] and not checkwin:
+						checkwin = True
+						total_games[pair]['wins'] += 1
 
 	for pair in averages:
 		for summ in averages[pair]:
 			for stat in averages[pair][summ]:
-				averages[pair][summ][stat] = averages[pair][summ][stat] / (total_games[pair] or 1)
+				averages[pair][summ][stat] = averages[pair][summ][stat] / (total_games[pair]['total'] or 1)
 
+	for pair in total_games:
+		total_games[pair]['durations'] = total_games[pair]['durations'] / (total_games[pair]['total'] or 1)
+		total_games[pair]['winrate'] = total_games[pair]['wins'] / (total_games[pair]['total'] or 1)
+
+
+	print('num games played and avg durations')
 	print(convert(total_games))
+	print('champions played')
 	print(convert(champs_played))
+	print('average stats:')
 	print(convert(averages))
+	# print('oldest game:')
+	# print(matches[oldest_game_id])
 
+# print(get_champion(99))
 run()
